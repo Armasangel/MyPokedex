e7d923b45b0a31defd527fbe53cad6b1aa7326f2
Angel <armasangel193@gmail.com>
2025-08-29 11:54:02 -0600

    Se realizo el Lab4

---
+++ app/src/main/java/com/uvg/mypokedex/ui/detail/DetailScreen.kt
@@ -0,0 +1,54 @@
+package com.uvg.mypokedex.ui.detail
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.uvg.mypokedex.data.model.Pokemon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+
+
+@Composable
+fun DetailScreen(
+    itemTitle: String,
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopBar(title = itemTitle, onBackClick = onBackClick)
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            Text(
+                text = "Detalle de $itemTitle",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        }
+    )
+}
