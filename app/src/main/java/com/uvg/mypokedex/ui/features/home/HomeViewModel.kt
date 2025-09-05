650bfe5523df65b1a5b8b8edbfb97d858e9e90de
Angel <armasangel193@gmail.com>
2025-09-05 13:43:21 -0600

    unu

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
@@ -4,21 +4,6 @@ import androidx.lifecycle.ViewModel
 import com.uvg.mypokedex.data.model.Pokemon
 
 class HomeViewModel : ViewModel() {
-    private val _pokemonList = mutableListOf(
-        Pokemon(1, "bulbasaur", "grass", 6.9f, "0.7"),
-        Pokemon(4, "charmander", "fire", "8", "0.61"),
-        Pokemon(7, "squirtle", "water", "9", "0.5"),
-        Pokemon(25, "pikachu", "electric", "6", "0.4"),
-        Pokemon(150, "mewtwo", "psychic", ),
-        Pokemon(151, "mew", "psychic"),
-        Pokemon(2, "ivysaur", "grass"),
-        Pokemon(3, "venusaur", "grass"),
-        Pokemon(5, "charmeleon", "fire"),
-        Pokemon(6, "charizard", "fire"),
-        Pokemon(8, "wartortle", "water"),
-        Pokemon(9, "blastoise", "water"),
-        Pokemon(10, "caterpie", "bug")
-    )
 
     val pokemonList: List<Pokemon>
         get() = _pokemonList
