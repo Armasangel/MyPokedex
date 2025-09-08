bf41c77e300f1b4d08430b399c4c0a78195e8fae
Armasangel <angel.armasher@gmail.com>
2025-09-07 20:41:41 -0600

    Update HomeViewModel.kt

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/HomeViewModel.kt
@@ -1,54 +1,60 @@
 package com.uvg.mypokedex.ui.features.home
 
-import com.uvg.mypokedex.data.model.PokeType
+import android.content.Context
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.uvg.mypokedex.data.model.Pokemon
+import kotlinx.serialization.json.Json
+import org.json.JSONObject
+import java.io.FileNotFoundException
 
-class HomeViewModel {
-    fun getPokemonList(): List<Pokemon> {
-        return listOf(
-            Pokemon(
-                id = 0001,
-                name = "Bulbasaur",
-                height = 0.7,
-                weight = 6.9,
-                category = "Semilla",
-                abilities = "Espesura",
-                gender = "Macho, Hembra",
-                type = listOf(PokeType.GRASS),
-                weaknesses = "Fuego · Hielo · Volador · Psíquica"
-            ), Pokemon(
-                id = 0007,
-                name = "Squirtle",
-                height = 0.5,
-                weight = 9,
-                category = "Tortuguita",
-                abilities = "Torrente",
-                gender = "Macho, Hembra",
-                type = listOf(PokeType.WATER),
-                weaknesses = "Eléctrico · Planta"
-            ), Pokemon(
-                id = 0004,
-                name = "Charmander",
-                height = 0.61,
-                weight = 8,
-                category = "Lagartija",
-                abilities = "Mar Llamas",
-                gender = "Macho, Hembra",
-                type = listOf(PokeType.FIRE),
-                weaknesses = "Agua · Eléctrico · Roca"
-            ), Pokemon(
-                id = 0025,
-                name = "Pikachu",
-                height = 0.4,
-                weight = 6.0,
-                category = "Ratón",
-                abilities = "Estática",
-                gender = "Macho, Hembra",
-                type = listOf(PokeType.ELECTRIC),
-                weaknesses = "Tierra"
-            )
-            )
-        )
+class HomeViewModel(private val context: Context) {
+    
+    private val _pokemonList = mutableStateListOf<Pokemon>()
+    val pokemonList: SnapshotStateList<Pokemon> = _pokemonList
+    private var currentPage = 0
+    private val json = Json { 
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+    
+    init {
+
+        loadMorePokemon()
+    }
+    
+
+    fun loadMorePokemon() {
+        val fileName = generateFileName(currentPage)
+        val newPokemon = loadPokemonFromJson(context, fileName)
+        
+        if (newPokemon.isNotEmpty()) {
+            _pokemonList.addAll(newPokemon)
+            currentPage++
+        }
+    }
+    
+
+    private fun generateFileName(page: Int): String {
+        val startId = (page * 10) + 1
+        val endId = startId + 9
+        return "pokemon_${startId.toString().padStart(3, '0')}_${endId.toString().padStart(3, '0')}.json"
+    }
+    
+    private fun loadPokemonFromJson(context: Context, fileName: String): List<Pokemon> {
+        try {
+            val jsonString: String = context.assets.open(fileName)
+                .bufferedReader()
+                .use { it.readText() }
+            val jsonObject = JSONObject(jsonString)
+            val itemsArrayString = jsonObject.getJSONArray("items").toString()
+            return json.decodeFromString<List<Pokemon>>(itemsArrayString)
+        } catch (e: FileNotFoundException) {
+            println("Archivo no encontrado: $fileName - Fin de los datos alcanzado")
+            return emptyList()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return emptyList()
+        }
     }
 }
-// Se agradece formalmente a Denil Parada por su paciencia y guía para dicho código
