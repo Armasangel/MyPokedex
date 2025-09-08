b07dbb8e95ce7f45962e39fec05c895af404ad90
Ronald <romanuelcatun13@gmail.com>
2025-09-07 20:50:34 -0600

    Update Pokemon.kt

--- app/src/main/java/com/uvg/mypokedex/data/model/Pokemon.kt
+++ app/src/main/java/com/uvg/mypokedex/data/model/Pokemon.kt
@@ -1,5 +1,8 @@
 package com.uvg.mypokedex.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Pokemon(
     val id: Int,
     val name: String,
@@ -7,10 +10,10 @@ data class Pokemon(
     val weight: Float,
     val height: Float,
     val stats: List<PokemonStat>,
-    val isFavorite: Boolean = false,
     val imageUrl: String = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"
 )
 
+@Serializable
 data class PokemonStat(
     val name: String,
     val value: Int,
