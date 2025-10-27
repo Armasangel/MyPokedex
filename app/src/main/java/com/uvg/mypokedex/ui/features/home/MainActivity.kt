80f45cd9916412d3132412a77cd316d551630ae1
Ronald <romanuelcatun13@gmail.com>
2025-10-26 23:09:15 -0600

    Refactor MainActivity to simplify navigation setup

--- app/src/main/java/com/uvg/mypokedex/ui/features/home/MainActivity.kt
+++ app/src/main/java/com/uvg/mypokedex/ui/features/home/MainActivity.kt
@@ -1,13 +1,9 @@
-package com.uvg.mypokedex
+package com.uvg.mypokedex.ui.features.home
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import com.uvg.mypokedex.navigation.AppNavigation
 import com.uvg.mypokedex.ui.theme.MyPokedexTheme
 
@@ -17,14 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyPokedexTheme {
-                val navController = rememberNavController()
-                
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavigation(
-                        navController = navController,
-                        paddingValues = innerPadding
-                    )
-                }
+                AppNavigation()
             }
         }
     }
